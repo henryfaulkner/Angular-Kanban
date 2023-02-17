1b6154f46a25ae0e8632f4bf71f6e08546f3f8f7
henryfaulkner <hfaulkner27@gmail.com>
2023-02-17 16:07:48 -0500

    up to the router video

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    NgbModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShellComponent } from './components/shell/shell.component';
+import { RouterModule } from '@angular/router';
+import { LayoutModule } from '@angular/cdk/layout'
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+const components = [
+  ShellComponent
+];
+const modules = [
+  CommonModule,
+  RouterModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule,
+  LayoutModule,
+  MatSidenavModule,
+  MatListModule,
+  MatMenuModule,
+  MatIconModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSnackBarModule,
+  LayoutModule
+];
+
+@NgModule({
+  declarations: [...components],
+  imports: [...modules],
+  exports: [
+    ...components,
+    ...modules
+  ]
+})
+export class SharedModule { }
